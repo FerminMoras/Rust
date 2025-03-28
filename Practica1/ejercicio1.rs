780b2b5eb2b56340d5b78d4683c831990de39196
FerminMoras <feer.moras@gmail.com>
2025-03-28 20:44:32 -0300

    Ejercicio 1 resuelto

--- Practica1/ejercicio1.rs
+++ Practica1/ejercicio1.rs
@@ -1,3 +1,20 @@
+use std::io::stdin;
+
 fn main() {
- println!("Seminario Rust 2024!");
+    let x: f32 = 10.5;
+    let mut num = String::new();
+
+    println!("Ingrese un numero real");
+    stdin().read_line(&mut num).expect("Error al leer");
+    let numero: f32 = num.trim().parse().expect("Ingrese un numero valido");
+
+    let suma = x + numero;
+    let multiplicacion = x * numero;
+    let resta = x - numero;
+    let division = x / numero;
+
+    println!("Suma: {}", suma);
+    println!("Resta: {}", resta);
+    println!("Multiplicacion: {}", multiplicacion);
+    println!("Division: {}", division);
 }
